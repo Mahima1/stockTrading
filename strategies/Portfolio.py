9704bf09af56e4f7dce6b5b8a8b41dd24abbc91c
Mahima Jain <mahimajain3110@gmail.com>
2019-07-03 23:30:06 +0530

    Refactored code

--- strategies/Portfolio.py
+++ strategies/Portfolio.py
@@ -12,39 +12,29 @@ class Portfolio(Main):
         t2['sigbinary'] = np.where(t2['signal'] == 'buy', 1, -1)
         t2['shifted'] = t2['sigbinary'].shift(1)
         t2['multi'] = t2['sigbinary'] * t2['shifted']
-        # return t2
-        # print ("shape is: ",t2.shape)
         t2['multi'].iloc[0] = -1
         t = t2[t2['multi'] == -1]
         mask = t['sigbinary'] == 1
         t['exec'] = np.where(mask, 'buy', 'sell')
         lastclose = float(t[-1:]['Close'])
         t = t.drop(columns=['shifted', 'multi', 'sigbinary'])
-        # Portfolio.printpf(t,0)
         if t['exec'].iloc[0] == 'buy':
-            # print("inside first buy signal df\n")
             r = t.shape[0]
             for i in range(r):
                 if t['exec'].iloc[i] == 'buy':
                     Portfolio.updatepf(0, Main.money / t[dfcol].iloc[i], 'buy')
-                    # Portfolio.printpf(t,i)
                 if t['exec'].iloc[i] == 'sell':
                     Portfolio.updatepf(Main.stocks * t[dfcol].iloc[i], 0, 'sell')
-                    # Portfolio.printpf(t,i)
 
         if t['exec'].iloc[0] == 'sell':
-            # print("inside first buy signal df")
             t = t[1:]
             r = t.shape[0]
             for i in range(r):
                 if t['exec'].iloc[i] == 'buy':
                     Portfolio.updatepf(0, Main.money / t[dfcol].iloc[i], 'buy')
-                    # Portfolio.printpf(t,i)
                 if t['exec'].iloc[i] == 'sell':
                     Portfolio.updatepf(Main.stocks * t[dfcol].iloc[i], 0, 'sell')
-                    # Portfolio.printpf(t,i)
         return Main.value(lastclose)
-        # return t
 
     def updatepf(moneyvalue, stocksvalue, sig):
         if sig == 'sell':
