07080247cc0d82dd2fc3dc9e1b993581e871b83a
Mahima Jain <mahimajain3110@gmail.com>
2019-07-03 15:45:20 +0530

    __init__.py file added

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from strategies.Rsi import Rsi
+from strategies.Boll import Boll
+from strategies.MACD import MACD
+from strategies.MA import MA
+from strategies.MAOMA import MAOMA
+from strategies.Vol import Vol
+from strategies.Join import join,optimizer
